ea628aa70ce16953d8c0ed059ff2c5e925555f43
Ryan Moradas Viajedor <viajedor.ryan1990@gmail.com>
2020-04-06 16:44:10 +0800

    Pagination, Order & Filter

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
 def index(request):
     listings = Listing.objects.all()
+    paginator = Paginator(listings, 6)
+
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
     template = 'listings/listings.html'
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, template, context)
 
