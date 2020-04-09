fe3d898eb52064cdbe391a8a8baa3a974cd09a16
Ryan Moradas Viajedor <viajedor.ryan1990@gmail.com>
2020-04-09 21:20:11 +0800

    Added Register & Login Templates

--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def register(request):
+        
     template = 'accounts/register.html'
     context = {
     }
-    return render(request, template, context)
+    if request.method == 'POST':
+        # TODO Register User
+        return
+    else:
+        return render(request, template, context)
 
 def login(request):
     template = 'accounts/login.html'
     context = {
     }
-    return render(request, template, context)
+    if request.method == 'POST':
+        # TODO Login User
+        return
+    else:
+        return render(request, template, context)
     
 def logout(request):
     return redirect('index')
